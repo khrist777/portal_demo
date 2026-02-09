ddc216969b167abf00cc3ab2516e3d736971f2cc
K-Khrist <khrist@matina.io>
2026-02-09 10:16:30 +0400

    Regenerate built assets and resource manifests
    Rebuild of compiled assets and manifests: replaces old hashed asset files, updates main and resources config/index files, and refreshes import chunks and native resources (images, font). Also updates index.html, adds new main bundle (main.c192d.js) and native assets, and renames the built settings file in src. This is a tidied build output reflecting updated asset hashes and packed resources.

--- src/settings.0e917.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/LobbyScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"d31e8",main:"16a7a"}};
---
+++ src/settings.becd4.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/LobbyScene.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",resources:"a6890",main:"ec5e0"}};
